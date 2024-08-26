928d6f54cb90725c25c7fce7e21c4a5cb3914aee
Dinesh <dinesh.shanmugaraj@walmart.com>
2024-08-26 19:55:42 +0530

    second

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dineshs90/demo1
 
 go 1.23.0
+
+require github.com/dineshs90/demo2 v0.0.0-20240826141522-526fdd74e628 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	test "github.com/dineshs90/demo2"
+)
 
 func main() {
 
 	fmt.Println("Test")
+	s1 := test.Bark()
+	s2 := test.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
 }
